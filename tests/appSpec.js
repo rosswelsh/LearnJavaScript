fd3378b4bf79eaca350a8851eed03f66a1a335f5
20007287 <rosswelsh@hotmail.com>
2016-12-08 11:59:38 +0000

    finish for today

--- tests/appSpec.js
+++ tests/appSpec.js
@@ -2,8 +2,18 @@
  * Created by davem on 27/11/2016.
  */
 
+'use strict';
+var learnjavascript={};
+
+learnjavascript.showView = function (hash){
+    var questionView =$ ('<div class ="question-view">').text('Arriving Soon!');
+    ${'view-container').empty().append(questionView);
+}
+
 describe('LearnJavascript', function (){
     it('can show question view',function () {
+        Learnjavascript.showView('#question-1');
+        expect($('.view-container.question-view').length).toEqual(1);
 
-    }};
-}};
+    });
+});
